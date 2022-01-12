2666eda7e76ccb467b13df1d8e97c298ab30c678
joshuacarvalho <59043004+joshuacarvalho@users.noreply.github.com>
2022-01-12 15:37:02 -0800

    iterate over final dataframe to check values

--- csv main.py
+++ csv main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import os
 
 """"#the os.listdir command tells you what files are in the available directory
@@ -24,8 +25,41 @@ biomarker_df = pd_cp_dataset.merge(pd_neo_dataset, on=cp_neo_merge_list)
 final_merge_list = ['month', 'year', 'subj']
 final_df = pd.merge(biomarker_df, pd_behav_dataset, how='left', on=final_merge_list)
 
+#filter the final DF to subject, sample number, and month
+#count how many samples per subject per month
+filter_list = ['subj', 'sample_number', 'month']
+final_df_checkker = final_df.filter(items=filter_list)
+final_df_checkker = final_df_checkker.sort_values(by=['month', 'subj'], ascending=True)
+#make a list of all different subj values
+unique_values = final_df_checkker.subj.unique()
+print(type(unique_values))
+#iterate over each unique values
 
-print(final_df.columns)
+#creates empty dataframe with each month and subject type
+df = pd.DataFrame(columns=[unique_values], index=[1,2,3,4,5,6,7,8,9,10,11,12])
+#appends data
+df.loc['1','allo'] = [1]
+print(df)
+
+#change range to (1,13) for each month
+for month in range(1,13):
+    for x in unique_values:
+        # creates custom query for specific value and month
+        query1 = "subj == '" + x + "' and month == " + str(month)
+        #queries DF for specific subject and month
+        #essentially final_df_checkker.query("subj == 'amos' and month ==1")
+        z = final_df_checkker.query(query1)
+
+
+        print(z, end=' \n')
+        #counts number of rows and therefore samples in each sub DF
+
+        print(z.shape[0])
+
+
+
+#print(final_df_checkker)
+
 
 
 
