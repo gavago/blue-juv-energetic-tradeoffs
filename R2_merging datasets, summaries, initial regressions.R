f53a81e6f158cccea8af055683a6bd8e006b01de
Nicole Thompson Gonzalez <gavago@gmail.com>
2022-07-21 14:57:01 -0700

    set up pipeline for udata merge, then summarize udata and fgc by month, then merge behavior month

--- R2_merging datasets, summaries, initial regressions.R
+++ R2_merging datasets, summaries, initial regressions.R
@@ -3,71 +3,61 @@ library(tidyverse)
 # library(magrittr)
 # library(lubridate)
 # library(mgcv)
- library(lmerTest)
+library(lmerTest)
 
 load("data/neo-dataset-full.Rdata", verbose = T)
 load("data/cp-dataset-full.Rdata", verbose = T)
 load("data/behav-dataset-month.Rdata", verbose = T)
 load("data/fgc_data_by_sample.Rdata", verbose = T)
 
+
 # save each Rdata dataset to CV ----
-# write.csv(neo_data_full, file = "neo dataset full.csv", row.names = F)
-# write.csv(cp_raw, file = "cp dataset full.csv", row.names = F)
-# write.csv(behav_data_month, file = "behav dataset month.csv", row.names = F)
+#write.csv(neo_data_full, file = "neo-dataset-full.csv", row.names = F)
+# write.csv(cp_raw, file = "cp-dataset-full.csv", row.names = F)
+# write.csv(behav_data_month, file = "behav-dataset-month.csv", row.names = F)
 
 
-# merging neo, cp, and behavior ----
 
-# check that time format is same
-# x <- left_join(cp_raw, neo_data_full, by = "sample_number")
-# x %>%
-#   filter(time.x != time.y) %>%
-#   select(starts_with("time"))
-
-#check intersection names across all data frames
-# intersect(names(neo_data_full), names(cp_raw)) %>%
-#   intersect(names(behav_data_month))
-
-# x <- neo_data_full %>% arrange(sample_number)
-# y <- cp_raw %>% arrange(sample_number)
-# all(x$time == y$time) # amazing, if cp_raw and neo aren't saved on the same day then they're times aren't the same
-
-
-merged_data <- left_join( neo_data_full, cp_raw, by = intersect(names(neo_data_full), names(cp_raw))) %>%
-  left_join(., behav_data_month) %>%
+# Merging urine sample data (neo, cp, cr_resid)  -----
+merged_udata <- left_join( neo_data_full, cp_raw, by = intersect(names(neo_data_full), names(cp_raw))) %>%
   select(group, subj, date, age, year, month, time, sample_number, stdsg_CP, neo_sg, everything())
-nrow(merged_data) # 620
 
-View(merged_data)
-
-# check out how many values NA per variable, should def be zero for age and sex
-apply(merged_data, 2, function(x) sum(is.na(x)))
-
-#write.csv(merged_data,  file = "data/merged_data_no_lean_body_mass.csv", row.names = F)
 
 # Add lean body mass - calculating cr-sg resids ------
-merged_data <- read.csv("data/merged_data_no_lean_body_mass.csv", header = T)
 
-merged_data %>%
-  #filter(sex == "M") %>%
-  #filter(subj == "dame") %>%
-  ggplot(., aes(y = Cr, x = SG/100, color = sex)) +
+merged_udata %>%
+  ggplot(., aes(y = Cr, x = SG/1000, color = sex)) +
   geom_point() +
   geom_smooth(method = "lm")
 
 
-m <- merged_data %>%
-  lm(Cr ~ I(SG/100) + I((SG/100)^2), data = ., na.action = na.exclude)
+m <- merged_udata %>%
+  lm(Cr ~ I(SG/1000) + I((SG/1000)^2), data = ., na.action = na.exclude)
 resids <- as.numeric(resid(m))
 
-full_data <- merged_data %>%
+full_udata <- merged_udata %>%
   mutate(cr_resid = resids)
 
-str(full_data)
+str(full_udata)
 
-#write.csv(full_data, file = "data/full_dataset_juv_immune_energetics.csv", row.names = F)
+#write_csv(full_udata, file = "data/urine_sample_dataset_juv_immune_energetics.csv")
+
+# Summarize udata and fgc data by month ------
+
+### summarize & save udata -----
+full_udata <- read_csv("data/urine_sample_dataset_juv_immune_energetics.csv")
+
+str(full_udata)
+# group_by, summarize, 
 
 
+### summarize & save fgcs ------
+load("data/fgc_data_by_sample.Rdata", verbose = T)
+str(gc_raw)
+
+
+
+# Merge monthly data - urine, feces, behavior -----
 # Validation of cr_resid by relationship with age, sex, and c-peptide ------
 
 #sex == "M"
@@ -111,6 +101,38 @@ summary(crsg_cp_lm)
 library(lme4)
 lmer(neo_sg ~ sex + scale(age) + scale(gm) + scale(stdsg_CP) + +  (1|subj), data = full_data)
 
+
+
+# graveyard -----
+merged_data <- read.csv("data/merged_data_no_lean_body_mass.csv", header = T)
+
+
+# check that time format is same
+# x <- left_join(cp_raw, neo_data_full, by = "sample_number")
+# x %>%
+#   filter(time.x != time.y) %>%
+#   select(starts_with("time"))
+
+#check intersection names across all data frames
+# intersect(names(neo_data_full), names(cp_raw)) %>%
+#   intersect(names(behav_data_month))
+
+# x <- neo_data_full %>% arrange(sample_number)
+# y <- cp_raw %>% arrange(sample_number)
+# all(x$time == y$time) # amazing, if cp_raw and neo aren't saved on the same day then they're times aren't the same
+
+
+merged_data <- left_join( neo_data_full, cp_raw, by = intersect(names(neo_data_full), names(cp_raw))) %>%
+  left_join(., behav_data_month) %>%
+  select(group, subj, date, age, year, month, time, sample_number, stdsg_CP, neo_sg, everything())
+nrow(merged_data) # 620
+
+View(merged_data)
+
+# check out how many values NA per variable, should def be zero for age and sex
+apply(merged_data, 2, function(x) sum(is.na(x)))
+
+#write.csv(merged_data,  file = "data/merged_data_no_lean_body_mass.csv", row.names = F)
 
 # graveyard ----
 
