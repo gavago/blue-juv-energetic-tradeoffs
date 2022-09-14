8714aad605d0a4eaa677548057425859b00ed796
Nicole Thompson Gonzalez <gavago@gmail.com>
2022-09-14 02:50:15 -0700

    make separate script with mediation package analysis

---
+++ R4-mediation-lmer.R
@@ -0,0 +1,59 @@
+library(tidyverse)
+detach("package:lmerTest", unload = T)
+library(lme4)
+
+set.seed(288)
+full_data_month_mediate <- full_data_month %>% 
+  drop_na(avg_fgc, avg_stdsg_CP, avg_neo_sg)
+colSums(!is.na(full_data_month_mediate))
+
+# total effect iv has on dv plus covariates
+fit.totaleffect <- lmer(avg_neo_sg ~ 
+                          log2(avg_stdsg_CP) + 
+                          age + 
+                          sex + 
+                          (1|subj), 
+                        #family = Gamma("log")
+                        full_data_month_mediate)
+
+
+# effect of iv on mediator
+fit.mediator <- lmer(avg_fgc ~ 
+                       log2(avg_stdsg_CP) + 
+                       age +
+                       sex +
+                       (1|subj),
+                     #family = Gamma("log"),
+                     data = full_data_month_mediate)
+
+# effect of mediator on dv controlling for iv
+
+fit.dv <- lmer(avg_neo_sg ~ 
+                 log2(avg_fgc) + 
+                 log2(avg_stdsg_CP) +
+                 age +
+                 sex +
+                 (1|subj),
+               #family = Gamma("log"),
+               data = full_data_month_mediate)
+
+summary(fit.dv)
+summary(fit.totaleffect)
+
+results = mediate(fit.mediator, fit.dv, treat='log2(avg_stdsg_CP)', mediator='log2(avg_fgc)')
+
+summary(results)
+# Total Effect = effect of CP on Neo
+# ADE = effect of CP on Neo independent of FGC
+# ACME = effect of CP on Neo via mediator FGC
+
+plot(results)
+summary(results, output = "b")
+
+
+qqnorm(residuals(fit.mediator))
+qqline(residuals(fit.mediator))
+hist(residuals(fit.mediator))
+qqnorm(residuals(fit.mediator))
+qqline(residuals(fit.mediator))
+hist(residuals(fit.mediator))
