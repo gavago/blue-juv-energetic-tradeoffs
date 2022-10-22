b3d6f1d4aa1a6a238eb925cfa9f2cf7cb735d0c0
Nicole Thompson Gonzalez <gavago@gmail.com>
2022-10-22 05:27:09 -0700

    rename

--- 1b-prep_merging datasets-summaries.R
+++ 1b-prep_merging datasets-summaries.R
@@ -116,50 +116,6 @@ view(full_data_month)
 # save csv
 # write.csv(full_data_month,  file = "data/full_data_month_udata_fgc_behav.csv", row.names = F)
 
-# Validation of cr_resid by relationship with age, sex, and c-peptide ------
-
-#sex == "M"
-  # ggplot() +
-  # geom_point(data = full_data %>% filter(subj == "allo"), 
-  #            aes(x = age, y = cr_resid), color = "red") +
-  # geom_smooth(data = full_data %>% filter(subj == "allo"), 
-  #             aes(x = age, y = cr_resid), color = "red",
-  #             method = "lm") +
-  # geom_point(data = full_data %>% filter(subj == "brog"), 
-  #            aes(x = age, y = cr_resid), color = "green") +
-  # geom_smooth(data = full_data %>% filter(subj == "brog"), 
-  #             aes(x = age, y = cr_resid),color = "green",
-  #             method = "lm") +
-  # geom_point(data = full_data, 
-  #            aes(x = age, y = cr_resid), alpha = 0.1) +
-  # geom_smooth(data = full_data, 
-  #             aes(x = age, y = cr_resid),
-  #             method = "lm")
-
-full_data %>% 
-  ggplot() +
-  geom_point(aes(x = age, y = cr_resid, color = sex), alpha = 0.3) +
-  geom_smooth(aes(x = age, y = cr_resid, color = sex), method = "lm") +
-  theme_minimal()
-
-df_cr_resid %>%
-  ggplot(., aes(x = sex, y = cr_resid)) +
-  geom_point() +
-  geom_boxplot()
-
-full_data %>%
-  ggplot(., aes(y = cr_resid, x = stdsg_CP, color = sex)) +
-  geom_point() +
-  geom_smooth(method = "lm")
-
-crsg_cp_lm <- lmer(cr_resid ~ sex + scale(stdsg_CP) + scale(age) + (1|subj), data = full_data)
-qqnorm(residuals(crsg_cp_lm))
-summary(crsg_cp_lm)
-
-library(lme4)
-lmer(neo_sg ~ sex + scale(age) + scale(gm) + scale(stdsg_CP) + +  (1|subj), data = full_data)
-
-
 
 # Sample counts for methods ------
 full_udata <- read_csv("data/urine_sample_dataset_juv_immune_energetics.csv")
@@ -203,7 +159,5 @@ View(merged_data)
 apply(merged_data, 2, function(x) sum(is.na(x)))
 
 #write.csv(merged_data,  file = "data/merged_data_no_lean_body_mass.csv", row.names = F)
-
-# graveyard ----
 
 
