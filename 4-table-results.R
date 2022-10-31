60ff1a8be4c77f6c06506914cfb923d39863a67e
Nicole Thompson Gonzalez <gavago@gmail.com>
2022-10-30 19:44:00 -0700

    create

---
+++ 4-table-results.R
@@ -0,0 +1,17 @@
+
+
+
+# energetic costs neo
+mod_info_extract(cp_neo_glm_month)
+mod_info_extract(lbm_neo_lm_month)
+
+# - mod behavior in light of costs? 
+mod_info_extract(f_neo_lm_month)
+mod_info_extract(r_neo_lm_month)
+mod_info_extract(m_neo_lm_month)
+
+# energetic costs neo short term
+mod_info_extract(lbm_change_neo_lm_sample)
+
+# H2a energetic limitations neo
+mod_info_extract(neo_cp_glm_month)
