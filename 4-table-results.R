d701db2890eaef1d2937607d2ffcbd2a4a26eae5
Nicole Thompson Gonzalez <gavago@gmail.com>
2022-10-30 21:43:16 -0700

    create script to start tabling results

--- 4-table-results.R
+++ 4-table-results.R
@@ -1,17 +1,27 @@
 
+source("functions/function-mod-info-extract.R")
+load("models/energetic-costs-immune.Rdata", verbose = T)
+load("models/short-energetic-costs-immune.Rdata", verbose = T)
+load("models/energetic-constraints-immune.Rdata", verbose = T)
+load("models/mechanism-constraints-on-immune.Rdata", verbose = T)
 
-
-# energetic costs neo
+# A1 - energetic costs neo
 mod_info_extract(cp_neo_glm_month)
 mod_info_extract(lbm_neo_lm_month)
 
-# - mod behavior in light of costs? 
+# A1 - mod behavior in light of costs? 
 mod_info_extract(f_neo_lm_month)
 mod_info_extract(r_neo_lm_month)
 mod_info_extract(m_neo_lm_month)
 
-# energetic costs neo short term
-mod_info_extract(lbm_change_neo_lm_sample)
+# A1 - energetic costs neo short term
+mod_info_extract(change_lbm_neo_lm_sample)
 
-# H2a energetic limitations neo
+# A2 - energetic limitations neo
 mod_info_extract(neo_cp_glm_month)
+
+# A2 - mechanism constraints
+
+
+
+
