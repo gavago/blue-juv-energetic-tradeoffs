9f460ff96967ff7f452cce07d5bd0da2f1b349d5
Nic Thompson Gonzalez <gavago@gmail.com>
2022-11-07 20:20:04 -0800

    add tabling script

--- 4-table-results.R
+++ 4-table-results.R
@@ -1,4 +1,4 @@
-
+library(tidyverse)
 source("functions/function-mod-info-extract.R")
 load("models/energetic-costs-immune.Rdata", verbose = T)
 load("models/short-energetic-costs-immune.Rdata", verbose = T)
@@ -6,16 +6,16 @@ load("models/energetic-constraints-immune.Rdata", verbose = T)
 load("models/mechanism-constraints-on-immune.Rdata", verbose = T)
 
 # A1 - energetic costs neo
-mod_info_extract(cp_neo_glm_month)
-mod_info_extract(lbm_neo_lm_month)
+costs1 <- mod_info_extract(cp_neo_glm_month)
+costs2 <- mod_info_extract(lbm_neo_lm_month)
 
-# A1 - mod behavior in light of costs? 
+# A1 - energetic costs neo short term
+costs3 <- mod_info_extract(change_lbm_neo_lm_sample)
+
+# A1 - modify behavior in light of costs? (Supplementary) 
 mod_info_extract(f_neo_lm_month)
 mod_info_extract(r_neo_lm_month)
 mod_info_extract(m_neo_lm_month)
-
-# A1 - energetic costs neo short term
-mod_info_extract(change_lbm_neo_lm_sample)
 
 # A2 - energetic limitations neo
 mod_info_extract(neo_cp_glm_month)
@@ -24,4 +24,5 @@ mod_info_extract(neo_cp_glm_month)
 summary(med_results)
 
 
-
+tab1_costs <- do.call("rbind", list(costs1, costs2, costs3))
+write.table(tab1_costs, file = "results/tables/tab1-costs.txt", quote = FALSE, sep = "/", row.names = F)
