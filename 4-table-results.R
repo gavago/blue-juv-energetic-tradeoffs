dce310cfd4250ae73ab625290a64df931dc66c83
Nicole Thompson Gonzalez <gavago@gmail.com>
2022-11-08 08:35:13 -0800

    returning to start after test

--- 4-table-results.R
+++ 4-table-results.R
@@ -5,8 +5,6 @@ load("models/short-energetic-costs-immune.Rdata", verbose = T)
 load("models/energetic-constraints-immune.Rdata", verbose = T)
 load("models/mechanism-constraints-on-immune.Rdata", verbose = T)
 
-#testing a change
-
 # A1 - energetic costs neo
 costs1 <- mod_info_extract(cp_neo_glm_month)
 costs2 <- mod_info_extract(lbm_neo_lm_month)
