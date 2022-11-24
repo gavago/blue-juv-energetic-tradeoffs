2afbfbf35eff358c3d22f154a5d5989b11d16027
Nicole Thompson Gonzalez <gavago@gmail.com>
2022-11-24 09:18:06 -0700

    save cp time effects

--- 1-prep-dataset-scripts/1b-time-of-day-effects.R
+++ 1-prep-dataset-scripts/1b-time-of-day-effects.R
@@ -1,35 +1,48 @@
 library(tidyverse)
 library(lmerTest)
 
-load("data/urine_sample_dataset_juv_immune_energetics.Rdata", verbose = T)
+load("data/neo-dataset-full.Rdata", verbose = T)
+load("data/cp-dataset-full.Rdata", verbose = T)
 load("data/fgc_data_by_sample.Rdata", verbose = T)
 
-as.numeric(full_udata$time)
-max(gc_raw$time)
-str(gc_raw)
-#full_udata$time
-#full_data$time <- unclass(as.POSIXct(full_udata$time))
 
-full_udata$hrs_midnight <- format(as.POSIXct(full_udata$time), format = "%H:%M:%S") %>%
+# create hrs since midnight var ------
+neo_data_full$hrs_midnight <- format(as.POSIXct(neo_data_full$time), format = "%H:%M:%S") %>%
+  lubridate::hms(.) %>%
+  as.numeric()/60/60 #as seconds, then divide into minutes and hours.
+head(neo_data_full$hrs_midnight)
+head(neo_data_full$time)
+
+cp_raw$hrs_midnight <- format(as.POSIXct(cp_raw$time), format = "%H:%M:%S") %>%
+  lubridate::hms(.) %>%
+  as.numeric()/60/60 #as seconds, then divide into minutes and hours.
+head(cp_raw$hrs_midnight)
+head(cp_raw$time)
+
+gc_raw$hrs_midnight <- format(as.POSIXct(gc_raw$time), format = "%H:%M:%S") %>%
 lubridate::hms(.) %>%
 as.numeric()/60/60 #as seconds, then divide into minutes and hours.
+head(gc_raw$hrs_midnight)
+head(gc_raw$time)
 
-
-full_udatax <- full_udata %>%
-  mutate(time = strptime(time, format = "%H:%M"))
-
-# time effects -----
-neo_time <- glmer(neo_sg ~ hrs_midnight + (hrs_midnight|subj), family = Gamma("log"), data = full_udata)
+# time effects regressions -----
+neo_time <- glmer(neo_sg ~ hrs_midnight + (hrs_midnight|subj), family = Gamma("log"), data = neo_data_full)
 summary(neo_time)
-cp_time <- glmer(stdsg_CP ~ hrs_midnight + (hrs_midnight|subj), family = Gamma("log"), data = full_udata)
+cp_time <- glmer(stdsg_CP ~ hrs_midnight + (hrs_midnight|subj), family = Gamma("log"), data = cp_raw)
 summary(cp_time)
+gc_time <- glmer(fgc.ng_g.feces ~ hrs_midnight + (hrs_midnight|subj), family = Gamma("log"), data = gc_raw)
+summary(gc_time)
 
-# viz time effects ----- 
+# sig time effects for cp
+
+
+# viz time effects for cp ----- 
 plot(full_udata$time, full_udata$neo_sg)
 plot(full_udata$time, full_udata$stdsg_CP)
 abline(lm(full_udata$stdsg_CP ~ full_udata$time))
 plot(tar$time, tar$cp_tar)
 abline(lm(tar$cp_tar ~ tar$time))
+
 #individ slopes cp ~ time
 full_udata %>%
   ggplot( aes(x=hrs_midnight, y=stdsg_CP, color = subj)) +
@@ -37,18 +50,13 @@ full_udata %>%
   geom_smooth(method = "lm", se = F, alpha = 0.5) +
   theme(legend.position = "none")
 
-# 
+# calculate predictions from cp-time -----
 
+cp_time_intercept <- colMeans(coef(cp_time)$subj)[1]
+cp_time_slope <- colMeans(coef(cp_time)$subj)[2]
+cp_time_pred <- exp(cp_time_intercept + cp_time_slope * cp_raw$hrs_midnight)
 
-intercept <- colMeans(coef(cp_time)$subj)[1]
-slope <- colMeans(coef(cp_time)$subj)[2]
-pred1 <- exp(intercept + slope * full_udata$hrs_midnight)
-
-tar <- full_udata %>%
-  mutate(cp_tar1 = stdsg_CP - pred1) %>%
-  mutate(cp_tar = cp_tar1 + abs(min(cp_tar1, na.rm = T)) + 0.0001)
-
-# compare to other predicted values ------
+# compare ran slop and intercept pred to other pred values ------
 
 tar <- full_udata %>% # create time adjusted residuals
   mutate(cp_tar1 = stdsg_CP - pred1) %>%
@@ -64,7 +72,8 @@ head(tar$cp_tar)
 tar_test <- glmer(cp_tar ~ hrs_midnight + (hrs_midnight|subj), family = Gamma("log"), data = tar)
 summary(tar_test) # no time effect now.
 
-# save cp time effects model ----- 
+# save cp time effects ----- 
 #to create time adjusted residual in 1b merge
-save(cp_time, file = "data/cp-time-of-day-model.Rdata")
+save(cp_time, cp_time_intercept, cp_time_slope, cp_time_pred,
+     file = "data/cp-time-of-day-model.Rdata")
 
