48f8681dca3ecba16b92c9710235f815ad3aa0b9
Nicole Thompson Gonzalez <gavago@gmail.com>
2022-11-23 21:42:58 -0700

    comments/to-do

--- 5-marginal-effects.R
+++ 5-marginal-effects.R
@@ -5,37 +5,29 @@ library(lmerTest)
 
 load("data/full_data_short_term_lbm_change.Rdata", verbose = T)
 load("data/full_data_month_udata_fgc_behav.RData", verbose = T)
+load("models/energetic-costs-immune-broad.Rdata", verbose = T)
 
-# marginal effects cp ~ neo ----
-cp_neo_glm_month <- glmer(avg_stdsg_CP ~ sex +
-                            age +
-                            mrank +
-                          log2(avg_neo_sg) + (1|subj),
-                          family = Gamma("log"), 
-                          data = full_data_month)
+# raw data points w marginal effects smooth, cp ~ neo ----
 
-cp_neo_glm_marg_effect <- ggemmeans(cp_neo_glm_month, terms = "avg_neo_sg")
+summary(cp_neo_glm_month)
+cp_neo_glm_marg_effect <- ggemmeans(cp_neo_glm_month, terms = "log2_avg_neo")
 
 cp_neo_glm_marg_effect
 
-ggplot(cp_neo_glm_marg_effect, aes(x, predicted)) +
-  geom_line(color = "deepskyblue4") +
-  geom_ribbon(aes(ymin = conf.low, ymax = conf.high), 
+
+ggplot() +
+  geom_point(data = full_data_month, aes(x = log2_avg_neo, y = avg_stdsg_CP)) +
+  geom_line(data = cp_neo_glm_marg_effect, aes(x, predicted), color = "deepskyblue4") +
+  geom_ribbon(data = cp_neo_glm_marg_effect, aes(x= x, y = predicted, ymin = conf.low, ymax = conf.high), 
               alpha = .3, 
               fill = "deepskyblue4") +
-  ylab("Predicted C-peptide")+
+  ylab("Predicted C-peptide") +
   xlab(bquote(Neopterin(log[2])))
- 
-# marginal effects lbm ~ neo ----
-lbm_neo_lm_month <- lmer(avg_cr_resid ~ 
-                           age +
-                           sex + 
-                           mrank +
-                           log2(avg_neo_sg) +
-                           (1|subj),
-                         data = full_data_month)
+# example of what each viz should do ^
+# color theme outcomes w points, minimal theme etc
 
-lbm_neo_marg_effect <- ggemmeans(lbm_neo_lm_month, terms = "avg_neo_sg")
+# marginal effects lbm ~ neo ----
+lbm_neo_marg_effect <- ggemmeans(lbm_neo_lm_month, terms = "log2_avg_neo")
 
 ggplot(lbm_neo_marg_effect, aes(x, predicted)) +
   geom_line(color = "coral3") +
@@ -46,15 +38,6 @@ ggplot(lbm_neo_marg_effect, aes(x, predicted)) +
   xlab(bquote(Neopterin(log[2])))
 
 # marginal effects short term lbm change ~ neo ----
-change_lbm_neo_lm_sample <-
-  lmer(sample_lbm_change ~ 
-         age + 
-         sex +
-         log2_neo +
-         sample_interval +
-         log2_neo*sample_interval +
-         (1|subj),
-       data = full_data_short_term_lbm_change)
 
 change_lbm_neo_marg_effect <- ggemmeans(change_lbm_neo_lm_sample, terms = "log2_neo")
 # NOTE: Results may be misleading due to involvement in interactions 
@@ -69,11 +52,13 @@ ggplot(change_lbm_neo_marg_effect, aes(x, predicted)) +
 # not logging predictor
 # no difference with interaction term removed
 
+
+# Supplemental exploratory ---------
 # marginal effects f ~ neo ----
-f_neo_lm_month <- lmer(f ~ sex + 
+r_neo_lm_month <- lmer(r ~ sex + 
                          age +
-                         log2(avg_neo_sg) + 
-                         log2(avg_stdsg_CP) +
+                         log2_avg_neo+ 
+                         log2_avg_stdsg_CP +
                          (1|subj), 
                        data = full_data_month)
 
