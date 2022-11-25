0f19006b90aa05eee651441493335b1101a307b1
Nicole Thompson Gonzalez <gavago@gmail.com>
2022-11-24 22:52:50 -0700

    new viz example, both logged

--- 5-marginal-effects.R
+++ 5-marginal-effects.R
@@ -6,6 +6,7 @@ library(lmerTest)
 load("data/full_data_short_term_lbm_change.Rdata", verbose = T)
 load("data/full_data_month_udata_fgc_behav.RData", verbose = T)
 load("models/energetic-costs-immune-broad.Rdata", verbose = T)
+load("models/energetic-costs-immune-short.Rdata", verbose = T)
 
 # raw data points w marginal effects smooth, cp ~ neo ----
 
@@ -14,16 +15,34 @@ cp_neo_lm_marg_effect <- ggemmeans(cp_neo_lm_month, terms = "log2_avg_neo")
 
 cp_neo_lm_marg_effect
 
-ggplot() + # eek doesn't seem to be on same scale anymore
-  geom_point(data = full_data_month, aes(x = log2_avg_neo, y = avg_cp_sg_tar)) +
+# both axes logged
+ggplot() + 
+  geom_point(data = full_data_month, aes(x = log2_avg_neo, y = log2_avg_cp_tar)) +
   geom_line(data = cp_neo_lm_marg_effect, aes(x, predicted), color = "deepskyblue4") +
   geom_ribbon(data = cp_neo_lm_marg_effect, aes(x= x, y = predicted, ymin = conf.low, ymax = conf.high), 
               alpha = .3, 
               fill = "deepskyblue4") +
-  ylab("Predicted C-peptide") +
-  xlab(bquote(Neopterin(log[2])))
+  ylab(expression(log[2]~"C-peptide" ~ "ng/ml")) +
+  xlab(expression(log[2]~Neopterin~"ng/ml")) +
+  theme_minimal()
 # example of what each viz should do ^
 # color theme outcomes w points, minimal theme etc
+
+# neo logged
+ggplot() + 
+  geom_point(data = full_data_month, aes(x = log2_avg_neo, y = avg_cp_sg_tar)) +
+  geom_line(data = cp_neo_lm_marg_effect, aes(x, 2^predicted), color = "deepskyblue4") +
+  geom_ribbon(data = cp_neo_lm_marg_effect, aes(x= x, y = 2^predicted, ymin = conf.low, ymax = conf.high), 
+              alpha = .3, 
+              fill = "deepskyblue4") +
+  ylab("C-peptide ng/ml (time-adjusted)") +
+  xlab(expression(log[2]~Neopterin~"ng/ml")) +
+  theme_minimal()
+
+# reminder why logging...
+# ggplot(full_data_month, aes(x = avg_neo_sg, y = avg_cp_sg)) +
+#   geom_point() +
+#   geom_smooth(method = "lm")
 
 # marginal effects lbm ~ neo ----
 lbm_neo_marg_effect <- ggemmeans(lbm_neo_lm_month, terms = "log2_avg_neo")
