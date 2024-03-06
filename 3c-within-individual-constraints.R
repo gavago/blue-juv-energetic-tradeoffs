22d816b5de1cdfa414dd050ec3dc5a73bbe18432
Nicole Thompson Gonzalez <gavago@gmail.com>
2024-03-06 15:35:36 -0800

    update within individual constraints

--- 3c-within-individual-constraints.R
+++ 3c-within-individual-constraints.R
@@ -18,13 +18,38 @@ neo_cons_fun1 <- function(x) { lm(log2_avg_neo ~
     summary() %>%
     coef()}
 
-full_data_month %>%
+wi_neo_cp_cr <- full_data_month %>%
   group_by(subj) %>%
   group_map(~ neo_cons_fun1(.x)) %>%
   do.call("rbind", .) %>%
   as.data.frame() %>%
   rownames_to_column() %>%
-  filter(grepl("log2_avg_cp_tar", rowname))
+  filter(grepl("log2_avg_cp_tar|cr_resid", rowname))
+
+
+# 30 of 41 within individual slopes neo ~ cp positive
+wi_neo_cp_cr %>%
+  filter(grepl("log2_avg_cp_tar", rowname)) %>%
+  mutate(is_pos = Estimate>0) %>%
+  count(is_pos)
+
+# mean estimate and mean std error of within individual slope neo ~ cp 0.16 ± 0.33
+wi_neo_cp_cr %>%
+  filter(grepl("log2_avg_cp_tar", rowname)) %>%
+  summarise(avg_neo_cp_slope = mean(Estimate), avg_SE = mean(`Std. Error`))
+
+
+# 33 of 41 within individual slopes neo ~ cr positive
+wi_neo_cp_cr %>%
+  filter(grepl("cr_resid", rowname)) %>%
+  mutate(is_pos = Estimate>0) %>%
+  count(is_pos)
+
+# mean estimate and mean std error of within individual slope neo ~ cr 1.8 ± 1.8
+wi_neo_cp_cr %>%
+  filter(grepl("cr_resid", rowname)) %>%
+  summarise(avg_neo_cp_slope = mean(Estimate), avg_SE = mean(`Std. Error`))
+
 
 # wihin sex - neopterin by energy balance and lean body mass ------
 
