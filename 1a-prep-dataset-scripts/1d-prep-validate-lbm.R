84c19265161fcb71edab23627a95ce129348270b
Nicole Thompson Gonzalez <gavago@gmail.com>
2024-08-01 13:25:52 -0700

    reorganize

--- 1a-prep-dataset-scripts/1d-prep-validate-lbm.R
+++ 1a-prep-dataset-scripts/1d-prep-validate-lbm.R
@@ -1,49 +1,50 @@
 library(tidyverse)
 library(lmerTest)
 
+
 load("data/urine_sample_dataset_juv_immune_energetics.Rdata", verbose = T)
 load("data/full_data_month_udata_fgc_behav.RData", verbose = T)
 
-# Validation - Cr influenced by monthly physical activity?
+# Validation - Cr resid -----
+
 names(full_data_month)
 
+# influenced by monthly physical activity? no
 cr_activity <- lmer(avg_cr_resid ~ log2(avg_cp_sg_tar) + age + m + f + r + (1|subj), data = full_data_month)
 summary(cr_activity)
 
-
-# Validation of cr_resid by relationship with age, sex, and c-peptide ------
-
-
-# Visualize
-full_data_month%>% 
-  ggplot() +
-  geom_point(aes(x = age, y =avg_ cr_resid, color = sex), alpha = 0.3) +
-  geom_smooth(aes(x = age, y =avg_ cr_resid, color = sex), method = "lm") +
-  theme_minimal()
-
-df_cr_resid %>%
-  ggplot(., aes(x = sex, y = avg_ cr_resid)) +
-  geom_point() +
-  geom_boxplot()
-
-full_data_month%>%
-  ggplot(., aes(y = avg_ cr_resid, x = stdsg_CP, color = sex)) +
-  geom_point() +
-  geom_smooth(method = "lm")
-
-# by sample
+# by sample - # increase w cp and age? yes
 lbm_by_age_samp <- lmer(cr_resid ~ sex + log2(cp_tar) + age + (1|subj), data = full_udata)
 qqnorm(residuals(lbm_by_age_samp))
 qqline(residuals(lbm_by_age_samp))
 summary(lbm_by_age_samp)
 
-# by monthly avg
+# by monthly avg - # increase w cp and age? yes
 lbm_by_age_mo <- lmer(avg_cr_resid ~ sex + log2_avg_cp_tar + age + (1|subj), data = full_data_month)
 summary(lbm_by_age_mo)
 
 # does monthly moving and resting predict monthly avg cr resid? no.
 # lbm_by_age_mo1 <- lmer(avg_cr_resid ~ sex + log2_avg_cp_tar + age + r + m + (1|subj), data = full_data_month)
 
+
+# Visualize - explore monthly cr_resid - paper figures in script 5 viz marginal effects -----
+full_data_month%>% 
+  ggplot() +
+  geom_point(aes(x = age, y =avg_cr_resid, color = sex), alpha = 0.5) +
+  geom_smooth(aes(x = age, y =avg_cr_resid, color = sex), method = "lm") +
+  theme_minimal()
+
+df_cr_resid %>%
+  ggplot(., aes(x = sex, y = avg_cr_resid)) +
+  geom_point() +
+  geom_boxplot()
+
+full_data_month %>%
+  ggplot(., aes(y = avg_cr_resid, x = avg_cp_sg_tar_viz, color = sex)) +
+  geom_point() +
+  geom_smooth(method = "lm")
+
+]
 
 # gravevyard ----
 sex == "M"
